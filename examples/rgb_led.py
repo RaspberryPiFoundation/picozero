6a58560291d31e7dfa1851afbc563921679a1b99
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2022-01-11 19:25:04 +0000

    Update rgb_led.py
    Updated to use new RGBLED class. More aligned with GPIO zero. Not convinced that toggle/blink should invert colours though.

--- examples/rgb_led.py
+++ examples/rgb_led.py
@@ -1,32 +1,32 @@
 # Common anode LED connected to GND and red = 2, green = 1, blue - 0
 from time import sleep
-from pico import RGBLED
+from picozero import RGBLED
 
-rgb = RGBLED()
+rgb = RGBLED(2, 1, 0)
 
 print("Red")
-rgb.color = (100, 0, 0)
+rgb.color = (1, 0, 0)
 
 sleep(2)
 
 print("Green")
-rgb.color = (0, 100, 0)
+rgb.color = (0, 1, 0)
 
 sleep(2)
 
 print("Blue")
-rgb.color = (0, 0, 100)
+rgb.color = (0, 0, 1)
 
 sleep(2)
 
 print("Pink")
-rgb.color = (100, 0, 50)
+rgb.color = (1, 0, 0.5)
 
 sleep(2)
 
-print("Disco")
+print("Blink")
+rgb.blink()
 
-while True:
-    rgb.random()
-    sleep(1)
+
+
 
