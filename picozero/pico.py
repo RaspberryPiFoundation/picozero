b236339f41b8e350f61582075bb2978c594b42f6
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2022-01-13 09:26:10 +0000

    Update pico.py

--- picozero/pico.py
+++ picozero/pico.py
@@ -134,6 +134,84 @@ class RGBLED(DigitalOutputDevice):
         r, g, b = self.value
         self.value = (1 - r, 1 - g, 1 - b)
         
+class AnalogInputDevice():
+    def __init__(self, pin, active_high=True, threshold=0.5):
+        self._adc = ADC(pin)
+        self.active_high = active_high
+        self.threshold = float(threshold)
+    
+    @property
+    def active_high(self):
+        return self._active_state
+
+    @active_high.setter
+    def active_high(self, value):
+        self._active_state = True if value else False
+        self._inactive_state = False if value else True
+        
+    @property
+    def value(self):
+        return self._read()
+
+    @value.setter
+    def value(self, value):
+        self._write(value)
+        
+    def _state_to_value(self, state):
+        return (state if self.active_high else 1 - state) / 65535
+
+    def _value_to_state(self, value):
+        return int(65535 * (value if self.active_high else 1 - value))
+    
+    def _read(self):
+        return self._state_to_value(self._adc.read_u16())
+        
+    @property
+    def threshold(self):
+        return self._threshold
+
+    @threshold.setter
+    def threshold(self, value):
+        self._threshold = float(value)
+
+    @property
+    def is_active(self):
+        return self.value > self.threshold
+
+    @property
+    def voltage(self):
+        return self.value * 3.3
+    
+    @property
+    def percent(self):
+        return int(self.value * 100)
+    
+class Potentiometer(AnalogInputDevice):
+    pass
+
+Pot = Potentiometer
+
+def onboard_temp_conversion(voltage):
+    # Formula for calculating temp from voltage for the onboard temperature sensor
+    return 27 - (voltage - 0.706)/0.001721
+
+class TemperatureSensor(AnalogInputDevice):
+    def __init__(self, pin, active_high=True, threshold=0.5, conversion=None):
+         self._conversion = conversion
+         super().__init__(pin, active_high, threshold)
+        
+    @property
+    def temp(self):
+        if self._conversion is not None:
+            return self._conversion(self.voltage)
+        else:
+            return None
+       
+onboard_temp_sensor = TemperatureSensor(4, True, 0.5, onboard_temp_conversion)
+TempSensor = TemperatureSensor
+Thermistor = TemperatureSensor
+
+# Old stuff below here
 
 onboardLEDPin = 25
 tempSensorADCPin = 4
@@ -332,7 +410,7 @@ class Speaker(PWM):
 ADCMAX = 65535
 VOLTAGECONVERSION = 3.3 / ADCMAX
 
-class Potentiometer(ADC):
+class PotentiometerOld(ADC):
     
     """
     TODO allow setting of observable min and max range (pots often don't get down to zero)
@@ -356,7 +434,7 @@ class Potentiometer(ADC):
     
 Pot = Potentiometer
         
-class TemperatureSensor(ADC):
+class TemperatureSensorOld(ADC):
     
     def __init__(self, adc=4, with_info=True):
         super().__init__(adc)
