9323e4c7c00dacf228a82faac1a90b4236b64c06
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2022-03-03 08:51:52 +0000

    Update speaker.py
    Updated to use play

--- docs/examples/speaker.py
+++ docs/examples/speaker.py
@@ -5,16 +5,17 @@ speaker = Speaker(5)
 
 def tada():
     c_note = 523
-    speaker.pitch(freq=c_note, duration=0.1)
+    speaker.play(c_note, 0.1)
     sleep(0.1)
-    speaker.pitch(c_note, 0.9)
+    speaker.play(c_note, 0.9)
 
 def chirp():
     global speaker
     for _ in range(5):
         for i in range(5000, 2999, -100):
-          speaker.pitch(i, 0.01)
+          speaker.play(i, 0.01)
         sleep(0.2)
+
 
 try: 
     tada()
