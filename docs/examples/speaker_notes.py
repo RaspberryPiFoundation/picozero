8d6765e119e9be71edb3642e13aca042b4de2ca2
beckyfranks <49150572+beckyfranks@users.noreply.github.com>
2022-03-08 09:12:55 +0000

    Update speaker_notes.py
    Changed the tune to a Swedish and Norwegian folk song called Liten Mus.

--- docs/examples/speaker_notes.py
+++ docs/examples/speaker_notes.py
@@ -5,12 +5,18 @@ speaker = Speaker(5)
 
 BEAT = 0.4
 
-tetris = [ ['e4', BEAT], ['b3', BEAT / 2], ['c4', BEAT / 2], ['d4', BEAT], ['c4', BEAT / 2], ['b3', BEAT / 2],  
-           ['a3', BEAT], ['a3', BEAT / 2], ['c4', BEAT / 2], ['e4', BEAT ],['d4', BEAT / 2], ['c4', BEAT / 2], 
-           ['b3', BEAT * 1.5], ['c4', BEAT / 2], ['d4', BEAT], ['e4', BEAT], ['c4', BEAT], ['a3', BEAT], ['a3', BEAT * 2]]
+liten_mus = [ ['d5', BEAT / 2], ['d#5', BEAT / 2], ['f5', BEAT], ['d6', BEAT], ['a#5', BEAT], ['d5', BEAT],  
+              ['f5', BEAT], ['d#5', BEAT], ['d#5', BEAT], ['c5', BEAT / 2],['d5', BEAT / 2], ['d#5', BEAT], 
+              ['c6', BEAT], ['a5', BEAT], ['d5', BEAT], ['g5', BEAT], ['f5', BEAT], ['f5', BEAT], ['d5', BEAT / 2],
+              ['d#5', BEAT / 2], ['f5', BEAT], ['g5', BEAT], ['a5', BEAT], ['a#5', BEAT], ['a5', BEAT], ['g5', BEAT],
+              ['g5', BEAT], ['', BEAT / 2], ['a#5', BEAT / 2], ['c6', BEAT / 2], ['d6', BEAT / 2], ['c6', BEAT / 2],
+              ['a#5', BEAT / 2], ['a5', BEAT / 2], ['g5', BEAT / 2], ['a5', BEAT / 2], ['a#5', BEAT / 2], ['c6', BEAT],
+              ['f5', BEAT], ['f5', BEAT], ['f5', BEAT / 2], ['d#5', BEAT / 2], ['d5', BEAT], ['f5', BEAT], ['d6', BEAT],
+              ['d6', BEAT / 2], ['c6', BEAT / 2], ['b5', BEAT], ['g5', BEAT], ['g5', BEAT], ['c6', BEAT / 2],
+              ['a#5', BEAT / 2], ['a5', BEAT], ['f5', BEAT], ['d6', BEAT], ['a5', BEAT], ['a#5', BEAT * 1.5]]
 
 try:
-    for note in tetris:
+    for note in liten_mus:
         speaker.play(note)
         sleep(0.1) # leave a gap between notes
        
