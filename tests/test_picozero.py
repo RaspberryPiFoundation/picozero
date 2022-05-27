75292f3ecba10cd5e1ccc5e4e10d77e3b0458750
Martin O'Hanlon <martin.ohanlon@raspberrypi.org>
2022-05-27 16:24:20 +0100

    added test

--- tests/test_picozero.py
+++ tests/test_picozero.py
@@ -126,6 +126,54 @@ class Testpicozero(unittest.TestCase):
         
         d.close()
 
+    def test_pwm_output_device_blink(self):
+        d = PWMOutputDevice(1)
+        
+        d.blink()
+        values = log_device_values(d, 1.1)
+        self.assertEqual(values, [1,0])
+        d.off()
+        self.assertFalse(d.value)
+        
+        d.blink(on_time=0.1, off_time=0.1, n=2)
+        values = log_device_values(d, 0.5)
+        self.assertEqual(values, [1,0,1,0])
+        self.assertFalse(d.value)
+
+        d.close()
+        
+    def test_pwm_output_device_pulse(self):
+        d = PWMOutputDevice(1)
+        
+        d.pulse(n=1)
+        values = log_device_values(d, 2.1)
+        
+        expected = [
+            0.0, 0.04, 0.08, 0.12, 0.16, 0.2, 0.24, 0.28, 0.32, 0.36, 0.4,
+            0.44, 0.48, 0.52, 0.56, 0.6, 0.64, 0.68, 0.72, 0.76, 0.8, 0.84,
+            0.88, 0.92, 0.96, 1.0, 0.96, 0.92, 0.88, 0.84, 0.8, 0.76,
+            0.72, 0.68, 0.64, 0.6, 0.56, 0.52, 0.48, 0.44, 0.4, 0.36, 0.32,
+            0.28, 0.24, 0.2, 0.16, 0.12, 0.08, 0.04, 0.0]
+        
+        if len(values) == len(expected):
+            for i in range(len(values)):
+                self.assertAlmostEqual(values[i], expected[i], places=2)
+        else:
+            self.fail(f"{len(values)} were generated, {len(expected)} were expected.")
+        
+        d.pulse(fade_in_time=0.5, fade_out_time=1, n=1, fps=4)
+        values = log_device_values(d, 2.1)
+        
+        expected = [0.0, 0.5, 1.0, 0.75, 0.5, 0.25, 0]
+        
+        if len(values) == len(expected):
+            for i in range(len(values)):
+                self.assertAlmostEqual(values[i], expected[i], places=2)
+        else:
+            self.fail(f"{len(values)} values were generated, {len(expected)} were expected.")
+        
+        d.close()
+
     def test_LED_factory(self):
         d = LED(1)
         self.assertIsInstance(d, PWMLED)
